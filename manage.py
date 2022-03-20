3b2eabedd6f9887ab707a65a5a311d0f3cf6de80
Huntscoding <101996622+Huntscoding@users.noreply.github.com>
2022-03-21 10:03:31 +1300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-import os
-import sys
+from flask import Flask, render_template
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+@app.route("/")
+def index():
+   return render_template("index.html")
 
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+if __name__ == '__main__':
+   app.run(debug = True)
